6d15d2228f893037ffc0f881e36e46a5d2767f2c
13072831201 <1971380780@qq.com>
2023-03-14 13:43:40 +0800

    提交源码

---
+++ cmake-build-debug/CMakeFiles/openjdk8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/openjdk8.dir/main.cpp.o"
+  "openjdk8.pdb"
+  "openjdk8"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/openjdk8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
